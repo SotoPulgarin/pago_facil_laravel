f05e032d471f5b3feeb843edbb20841af07f576a
SotoPulgarin <ahsoto@misena.edu.co>
2020-02-22 11:43:27 -0500

    aprendiendo arreglos

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,54 @@ Route::get('/', function () {
 
 
 //Primera ruta: mensaje
-Route::get('hola', function(){
+Route::get('hola', function () {
 
-    echo "hola madrugadores";
+    $ficha = 1753443;
+    echo 'hola madrugadores', $ficha;
+});
+
+
+//Ruta tratar arreglos
+
+Route::get('arreglo', function () {
+
+    //Crear arreglo:
+
+    $estudiantes = [
+        "A" => "ali",
+        "N" => "ana",
+        "L" => "lola"
+
+
+    ];
+    //var_dump : analiza una variable
+    //Para imprimir cada valor del arreglo:
+    // echo "<pre>";
+    // echo $estudiantes["A"];
+    // // echo "<hr />";
+    // echo $estudiantes["N"];
+    // // echo "<hr />";
+    // echo $estudiantes["L"];
+    // // echo "<hr />";
+    // // var_dump($estudiantes);
+    // echo "</pre>";
+
+    //Accedo al elemento del arreglo
+    // echo $estudiantes ["A"];
+
+    // //Asignar valor a un elemento del arreglo
+    // $estudiantes ["L"] = "Laura";
+
+    //Ciclo For each=especifico para recorrer arreglos, desde el primer elemento hasta el ultimo, de uno en uno
+
+    $estudiantes[] = "hassan";
+
+    foreach($estudiantes as $clave => $estudiantes){
+
+        echo "$clave - $estudiantes";
+        echo "<hr /r>";
+
+    };
+
 
 });
